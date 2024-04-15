4805a23c7d743e1f34d708afd32e0df6622f886f
Terry <madeinterry@gmx.us>
2024-04-14 20:37:39 -0700

    Initial commit and set up the Login Page

---
+++ src/components/Login.jsx
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import '../css/Login.css'
+
+document.title = 'Login | MaterDei Admin'
+
+let time = new Date().getHours()
+
+
+function Login() {
+    const [formData, setFormData] = useState({userName: '', password: ''})
+
+    function handleClick(event) {
+        setFormData(prev => {
+            return {userName: '', password: ''}
+        })
+        alert('The form was submitted')
+        event.preventDefault()
+    }
+
+    function handleChange(event) {
+        setFormData(prev => {
+            return {
+                ...prev,
+                [event.target.name]: event.target.value
+            }
+        })
+    }
+  return (
+    <div className="form-container">
+    <form action="">
+      <h1 className="intro">{time < 12 ? 'Mwaramutse' : 'Mwiriwe'} neza<span> {formData.userName}!</span></h1>
+
+      <div className="input-container">
+        <i className="fa fa-user-circle icon"></i>
+        <input
+          type="text"
+          className="input-field"
+          name="userName"
+          value={formData.userName}
+          placeholder="Enter Username"
+          onChange={handleChange}
+          required
+        />
+      </div>
+
+      <div className="input-container">
+        <i className="fa fa-key icon"></i>
+        <input
+          type="password"
+          className="input-field"
+          name="password"
+          value={formData.password}
+          placeholder="Password"
+          required
+          onChange={handleChange}
+        />
+      </div>
+
+      <button onClick={handleClick} className="login-btn">Login</button>
+    </form>
+  </div>
+  )
+}
+
+export default Login
